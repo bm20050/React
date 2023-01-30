20375abf44377f45918f7d449806c94c8ed646c2
bm20050 <bm20050@gmail.com>
2023-01-30 09:24:51 +0900

    01.30 update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import MyDiv1 from './01/MyDiv1'
-import MyDiv2 from './01/MyDiv2';
-import MyClock from './01/MyClock';
+import Box from './02/Box';
+
 const App = () => {
   return (
-    <div className="App-header">
-      <MyDiv1 />
-      <MyDiv2 />
-      <MyClock />
+    <div>
+      <Box />
     </div>
-
   );
 }
 
